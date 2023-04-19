6dabe619c0449f384af493ef3298b6c03aef28f1
Shivani Rajput <rajput.shivani@tftus.com>
2023-04-19 19:38:23 +0530

    Cloudifytests app

---
+++ src/Pages/ContactUs/ToastContainer.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { Button, Alert } from "@mui/material";
+import Snackbar from "@mui/material/Snackbar";
+
+export default function ToastContainer({ open, setOpenSnackbar, successData }) {
+  const handleClose = () => {
+    setOpenSnackbar(false);
+  };
+
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        open={open}
+        onClose={handleClose}
+        message="Something Went Wrong"
+      >
+        {successData ? (
+          <Alert variant="filled" severity="success">
+            your data submitted , soon we will get you
+          </Alert>
+        ) : (
+          <Alert variant="filled" severity="error">
+            Something went wrong
+          </Alert>
+        )}
+      </Snackbar>
+    </div>
+  );
+}
