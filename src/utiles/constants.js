1330fbe68e168e3de5af17e275d1c437fd7a0dbf
tanmay <agarwal.tanmay@tftus.com>
2023-08-24 16:36:30 +0530

    staging urls changed

--- src/utiles/constants.js
+++ src/utiles/constants.js
@@ -1,4 +1,4 @@
 // export const jira_api = process.env.REACT_APP_JIRA_VARIABLE;
 
 export const jira_api = `https://app.cloudifytests.io/contactusform`;
-export const free_trial_api = `https://app.cloudifytests.io/send-invite-mail/`;
+export const free_trial_api = `https://app.cloudifytests.com/send-invite-mail/`;
